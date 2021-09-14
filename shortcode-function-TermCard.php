5e726cc66f01efebf6f0e534f00b11efd9f3bce4
Alexandru <alex_apetrei@outlook.com>
2021-09-14 20:27:20 +0100

    Successfully integrated calendar with events calendar by knowledgebase plugin

--- shortcode-function-TermCard.php
+++ shortcode-function-TermCard.php
@@ -1,42 +1,50 @@
 <?php
     function Draw_TermCard() {
         $events = tribe_get_events();
- 
+
         $num_of_days = 56;
 
         // Create an empty array to write events into
         $events_array = array_fill(0, $num_of_days, array());
 
         $start_date_object = null;
-
+        
+        // Find the date on which the term starts
         foreach ( $events as $event ) {
             if ($event -> post_title == "Term Start") {
                 $details = tribe_get_event($event);
-                $start_date_object = $details -> start;
+
+                $start_date_object = new DateTime(date($details -> start_date));
                 break;        
             }
         }
+
         
-        if ( start_date_object ) {
+        if ( $start_date_object !== null ) {
             
             // Extract all details and place them in events_array, which is ordered by day
             foreach ( $events as $event ) {
                 $details = tribe_get_event($event);
                 
-                $event_date_object = $details -> start;
 
-                $diff = $start_date_object -> diff($event_date_object);
+                $diff = $start_date_object -> diff(new DateTime(date($details -> start_date)));
                 $days_diff = $diff -> days;
 
                 //Check if the event happens in the current term before placing it in the array
-                if (0 <= $days_diff && $days_diff < $num_of_days) {
-                    $start_time = substr($details -> start_date, -8);
-                    $end_time = substr($details -> end_date, -8);
-                    $link = new SimpleXMLElement(tribe_get_event_website_link($event));
+                if (0 <= $days_diff && $days_diff < $num_of_days && $event -> post_title !== "Term Start") {
+                    $start_time = substr($details -> start_date, -8,5);
+                    $end_time = substr($details -> end_date, -8,5);
+                    $unparsed_link = tribe_get_event_website_link($event);
+                    $link = "";
+                    if ($unparsed_link !== "") {
+                        $XML = new SimpleXMLElement(tribe_get_event_website_link($event));
+                        $link = $XML["href"];
+                    }
+                    
                     $categ = substr(strip_tags(tribe_get_event_categories($event -> ID)),16);
                     
                     $content = array("title" => $event -> post_title, "start_time" => $start_time, "end_time" => $end_time,
-                    "category" => $categ, "link" => $link["href"]);
+                    "category" => $categ, "link" => $link);
 
                     array_push($events_array[$days_diff], $content); 
                 }
@@ -46,28 +54,34 @@
             $html_output = "<figure class='wp-block-table alignfull'><table><tbody><tr><td></td><td>Sunday</td><td>Monday</td><td>Tuesday</td>
             <td>Wednesday</td><td>Thursday</td><td>Friday</td><td>Saturday</td>";
 
+            $day_count = 0;
             //Generate html
-            for ($i = 0; $i < $num_of_days; $i++) {
+            for ($i = 0; $i < 64; $i++) {
                 
-                if (i % 7 == 0) {
-                    $html_output .= "</tr><tr>";
+                if ($i % 8 == 0) {
+                    $html_output .= "</tr><tr><td>Wk". ($i+8)/8 . "</td>";
+                } else {
+                    $html_output .= "<td>";
+
+                    foreach ( $events_array[$day_count] as $event ) {
+                    $html_output .= "<div name= \"$event[category]\"> <a href=\"$event[link]\">$event[title]</a><br>
+                    $event[start_time]-$event[end_time]</div>";
+                    }
+
+                    $html_output .= "</td>";
+                    $day_count += 1;
                 }
 
-                $html_output .= "<td>";
-
-                foreach ( $events_array[i] as $event ) {
-                    $html_output .= "<div name= \"$event['category']\"> <a href=\"$event['link']\">$event['title']</a> <br> 
-                    ${event['start_time']}-$event['end_time']</div>";
-                }
-
-                $html_output .= "</td>";
             }
             
             $html_output .= "</tr></tbody></table></figure>";
 
+            return $html_output;
         }
+        
+
     }
 
-    add_shortcode( 'TermCard', 'Draw_TermCard');
+    add_shortcode( 'Term_Card', 'Draw_TermCard');
 
 ?>
